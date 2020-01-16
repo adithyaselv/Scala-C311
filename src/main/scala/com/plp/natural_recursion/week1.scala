b4e8db29daf77e83876d71cd5447edaa218e8bbd
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2020-01-16 12:24:21 -0500

    class 2 notes 01/16/2020

--- src/main/scala/com/plp/natural_recursion/week1.scala
+++ src/main/scala/com/plp/natural_recursion/week1.scala
@@ -67,4 +67,37 @@ object week1 extends App {
 
   println(member("cat", List("cat", "dog", "dog")))
 
+  def doubleArrow(n: Int, m: Int): Int = {
+    m match {
+      case 0 => 1
+      case _ => power(n, doubleArrow(n, m-1))
+    }
+  }
+
+  def G(k: Int)(n:Int,m: Int) : Int = {
+    k match {
+      case 0 => plus(n, m)
+      case i => m match {
+        case 0 => if (i==1) 0 else 1
+        case _ => G(k-1)(n, G(k)(n, m-1))
+      }
+    }
+  }
+
+  println(G(1)(3,5))
+
+  // Improve by removing if else and make it look like original definition
+  def ackermann(m:Int, n:Int):Int = {
+    if (m ==0) {
+      n + 1
+    }
+    else if (n==0) {
+      ackermann(m - 1, 1)
+    }
+    else
+      ackermann(m-1, ackermann(m, n-1))
+  }
+
+  println(ackermann(3,3))
+
 }
